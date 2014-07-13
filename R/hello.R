bb42a6a31e0e1e693ca40dde201eb5f8f270bafc
dexam <paulvanloon88@hotmail.com>
2014-07-13 14:46:10 +0100

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -15,3 +15,31 @@ x<-sqrt(as.numeric(myname))
     message = paste("hello, you entered", myname, " and the square root is ",x)
   )
 }
+
+
+
+
+
+hypotheek<-function(P=100000,I=5, L=30, T=30, A=0){
+P=P-A
+N = L *12
+J = I/(12*100)
+
+M = P * (J / (1-(1+J)^(-N)))
+
+H<-c();C<-c();Q<-c();i=1
+while(P > 0){
+H[i] = P * J
+C[i] = M - H[i]
+Q[i] = P - C[i]
+P = Q[i]
+
+i = i+1
+}
+
+NET = M-(H*(T/100))
+
+return(list(balance=Q, monthly_payments_bruto=M,monthly_payments_netto=NET, interest_payments=H, capital_payments=C))
+
+}
+
